bbb3d0680b0a3ca1ba2503024a7767f6badb63f7
Jonghwa Song <whdghk3471@gmail.com>
2023-06-26 03:08:37 +0900

    #3.4 Recap

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleHome = (req, res) => {
+  return res.end();
+};
+
+app.get("/", handleHome);
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
