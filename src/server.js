a3618607ddb986ee7c7a2a7bfab64f05f2e21f2a
Jonghwa Song <whdghk3471@gmail.com>
2023-06-27 03:12:56 +0900

    #3.11 finish

--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const handleHome = (req, res) => {
-  return res.end();
+const home = (req, res) => {
+  console.log("I will respond.");
+  return res.send("hello");
 };
 
-app.get("/", handleHome);
+const login = (req, res) => {
+  return res.send("login");
+};
+
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);
