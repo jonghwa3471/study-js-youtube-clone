002a5cfd9f9163720ddd52808c533b0cdd1158f4
Jonghwa Song <whdghk3471@gmail.com>
2023-07-04 11:52:44 +0900

    finish #4.8

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { watch, edit } from "../controllers/videoControllers";
+import {
+  see,
+  edit,
+  upload,
+  deleteVideo,
+} from "../controllers/videoControllers";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", watch);
-videoRouter.get("/edit", edit);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/upload", upload);
 
 export default videoRouter;
