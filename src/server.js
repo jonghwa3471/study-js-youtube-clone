cce4f300c06fcc43b9232e12cd5d5eae00e81322
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2023-08-05 18:32:23 +0900

    add emoji

--- src/server.js
+++ src/server.js
@@ -17,6 +17,6 @@ app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT} 🚀`);
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
