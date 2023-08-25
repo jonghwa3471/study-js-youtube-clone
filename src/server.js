147ae25ffa48f7e9ab719fae1183901d41a0309c
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2023-08-25 18:02:32 +0900

    finished to #6.18

--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
-import "./db";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
@@ -17,7 +15,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log(`✅ Server listening on http://localhost:${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
+export default app;
