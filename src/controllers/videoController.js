baf0336f74f242de6efef65ecddd3dc4a4f61a4a
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2023-09-05 15:30:12 +0900

    finished #6.24

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -33,9 +33,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(",")
-      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -50,7 +48,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags,
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
