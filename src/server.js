5e2b42a6e239ccdec11bc8dc8447c2d8f40d32b8
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2023-09-13 13:52:32 +0900

    started #7.0

--- src/server.js
+++ src/server.js
@@ -7,8 +7,8 @@ import videoRouter from "./routers/videoRouter";
 const app = express();
 const logger = morgan("dev");
 
-app.set("views", process.cwd() + "/src/views");
 app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
