5618abb9d9787602952ce84a1cbef80be5990222
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2023-10-22 18:08:31 +0900

    finished at #7.17

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -57,6 +57,20 @@ export const postLogin = async (req, res) => {
   req.session.user = user;
   return res.redirect("/");
 };
+
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: "005f235227afd9d39129",
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = (req, res) => {};
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const logout = (req, res) => res.send("Log out");
