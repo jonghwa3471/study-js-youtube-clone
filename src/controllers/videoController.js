b2d6722ade3404b9433d12ef637009e79088bc75
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2023-12-20 17:21:36 +0900

    finished until #8.9

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -43,11 +43,15 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = async (req, res) => {
-  const { title, description, hashtags } = req.body;
+  const {
+    body: { title, description, hashtags },
+    file: { path: fileUrl },
+  } = req;
   try {
     await Video.create({
       title,
       description,
+      fileUrl,
       hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
