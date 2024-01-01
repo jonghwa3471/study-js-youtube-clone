1e7ea391fdf4ba885a0f5bb1f212082d1e60ca1d
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-01-02 01:38:08 +0900

    finished until #10.3

--- src/db.js
+++ src/db.js
@@ -4,8 +4,8 @@ mongoose.connect(process.env.DB_URL);
 
 const db = mongoose.connection;
 
-const handleError = (error) => console.log("❌ DB Error", error);
 const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = (error) => console.log("❌ DB Error", error);
 
 db.on("error", handleError);
 db.once("open", handleOpen);
