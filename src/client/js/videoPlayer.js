b1712d719d9f9766b1b3107e71f8c5d38375c56f
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-01-05 18:43:49 +0900

    #11.3

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,9 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
 const handlePlayClick = (e) => {
   if (video.paused) {
     video.play();
@@ -16,12 +19,39 @@ const handlePlayClick = (e) => {
 const handleMute = (e) => {
   if (video.muted) {
     video.muted = false;
+    video.volume = volumeValue;
   } else {
     video.muted = true;
   }
   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-  volumeRange.value = video.muted ? 0 : 0.5;
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleInputVolumeRange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  if (value === "0") {
+    video.muted = true;
+    muteBtn.innerText = "Unmute";
+  }
+  video.volume = value;
+};
+
+const handleChangeVolumeRange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (value !== "0") {
+    volumeValue = value;
+  }
 };
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleInputVolumeRange);
+volumeRange.addEventListener("change", handleChangeVolumeRange);
