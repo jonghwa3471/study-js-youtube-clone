34c33ceb3bd188308bd69b8e55caf5cdbbc4f785
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-02-02 16:57:03 +0900

    PORT variable

--- src/init.js
+++ src/init.js
@@ -5,7 +5,7 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT;
 
 const handleListening = () =>
   console.log(`✅ Server listening on http://localhost:${PORT} 🚀`);
